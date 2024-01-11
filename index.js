0c3f9bac69fca9695f17041522719d6398bbd91a
Rohan Kadi <rkrohan174@gmail.com>
2024-01-11 14:13:39 +0530

    portfolio

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
-const navMenu=document.getElementById("nav_menu")
-const navToggle=document.getElementById("nav_toggle")
-const navClose=document.querySelector(".nav_close")
+const navMenu = document.getElementById("nav_menu")
+const navToggle = document.getElementById("nav_toggle")
+const navClose = document.querySelector(".nav_close")
 
-if(navToggle){
-    navToggle.addEventListener("click",()=>{
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
         navMenu.classList.add("show-menu")
     })
 }
-if(navClose){
-    navClose.addEventListener("click",()=>{
+if (navClose) {
+    navClose.addEventListener("click", () => {
         navMenu.classList.remove("show-menu")
     })
 }
 
 
 const modalViews = document.querySelectorAll(".services_modal"),
-      modalBtns=document.querySelectorAll(".services_button")
-      modalCloses=document.querySelectorAll(".services_modal-close")
+    modalBtns = document.querySelectorAll(".services_button")
+modalCloses = document.querySelectorAll(".services_modal-close")
 
-let modal=function(modalclick){
+let modal = function (modalclick) {
     modalViews[modalclick].classList.add("active-modal")
 }
 
-modalBtns.forEach((modalBtns,i)=>{
-    modalBtns.addEventListener("click",()=>{
+modalBtns.forEach((modalBtns, i) => {
+    modalBtns.addEventListener("click", () => {
         modal(i)
     })
 })
 
-modalCloses.forEach((e)=>{
-    e.addEventListener("click",()=>{
-        modalViews.forEach((el)=>{
+modalCloses.forEach((e) => {
+    e.addEventListener("click", () => {
+        modalViews.forEach((el) => {
             el.classList.remove("active-modal")
         })
     })
 })
+
+var typed = new Typed('.home_subtitle', {
+     strings: [ "Java Developer", "Frontend Developer", "Backend Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
